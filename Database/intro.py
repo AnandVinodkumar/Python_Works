21157e32cd984550e4ef5d161818c42982e5945d
Anand Vinodkumar <anandvkmv4@gmail.com>
2025-09-18 16:46:06 +0530

    hello1

---
+++ Database/intro.py
@@ -0,0 +1,33 @@
+"""
+Database:
+
+used to store the data well organized
+
+Type of data
+
+Text, Integer, Float, Boolean, Date/Time, Image, File, Audio, Video, etc
+
+Types of Databases:
+========================
+RDBMS: used to keep data in table format (text,integer,float,boolean,date/time)
+NoSQL: used to keep data in file format (file,image,audio,video)
+Graph DB
+Cloud DB
+
+
+data:
+==========
+Raw material which is kept in the database
+
+used to be processed and converted into information
+
+DBMS: Database Management System
+========================================
+It is a software used to create, read, update, delete the data in the database
+
+
+RDBMS: Relational Database Management System
+====================================================
+It is a software used to create, read, update, delete the data in the database in table format
+
+"""
